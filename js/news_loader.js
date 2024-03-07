8c3393a2edd5897a13c7e0fd8a4ea4da23fde6ef
owanr <risakoowan@gmail.com>
2024-03-06 20:39:10 -0600

    adding news.json file

--- js/news_loader.js
+++ js/news_loader.js
@@ -4,51 +4,23 @@ gtag('js', new Date());
 gtag('config', 'UA-166307709-1');
 
 
-function toggle_read_more() {
-    var moreText = document.getElementById("more_datalist_news");
-    var btnText = document.getElementById("read_more_button");
-    if (moreText.style.display === "none") {
-      moreText.style.display = "block";
-      btnText.textContent = "Read Less";
-    } else {
-      moreText.style.display = "none";
-      btnText.textContent = "Read More";
-    }
-  }
-
-var json_news_url = 'https://dykang.github.io/assets/publication.json'; // for testing
-// var json_news_url = 'file:///Users/risako/Desktop/assets/news.json '; // for the actual website
+var json_news_url = 'https://minnesotanlp.github.io/assets/news.json';
 $.getJSON(json_news_url, function foo(result) { render_news(result); });
 
 function render_news(elements=null, filter=null){
     var decoded_text = '';
-    var more_decoded_text = '';
     var counter = 0;
-    for (let i = 0; i < elements.length; i++) {
-        const value = elements[i];
+    $.each(elements, function (index, value) {
         counter += 1;
-
-        // for testing
-        if (value.year != null && value.title != null) {
-            var decoded_var = '<li>(' + value.year + ') ' + value.title + '</li>';
-
-        // for the actual website
-        // if (value.date != null && value.description != null){
-            // var decodedVar = '<li>(' + value.date + ') ' + value.description + '</li>';
+        if (value.date != null && value.description != null){
+            var decodedVar = '<li>(' + value.date + ') ' + value.description + '</li>';
         }
-        if (i < 3) {
-            decoded_text += decoded_var;
-        } else {
-            more_decoded_text += decoded_var;
-        }
-    }
-   
+        decoded_text += decoded_var;
+    });
     if (counter > 0){
         decoded_text = decoded_text ;
     }
-
     $('#datalist_news').append(decoded_text);
-    $('#more_datalist_news').append(more_decoded_text);
 }
 
 
